b00a4a06ac18c32bd81de31b17a0ba8e7057980d
JohT <johnnyt@gmx.at>
2022-06-04 09:16:55 +0200

    benchmark GitHub/JanWilczek/fir-simd

--- test/BenchmarkTest.cpp
+++ test/BenchmarkTest.cpp
@@ -1,8 +1,10 @@
+#include "../source/FIRFilter.h"
 #include "catch2/generators/catch_generators.hpp"
 #include "catch2/generators/catch_generators_range.hpp"
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
+#include <iostream>
 #include <iterator>
 #include <memory>
 #include <random>
@@ -10,36 +12,35 @@
 #include <sys/stat.h>
 
 // Reference: https://stackoverflow.com/questions/24518989/how-to-perform-1-dimensional-valid-convolution
-template<typename T>
-std::vector<T> convolution_full(std::vector<T> const &f, std::vector<T> const &g)
+template<typename SampleType>
+void convolution_full(const std::vector<SampleType> &in, const std::vector<SampleType> &kernel, std::vector<SampleType> out)
 {
-    auto const nf = f.size();
-    auto const ng = g.size();
-    auto const n = nf + ng - 1;
-    std::vector<T> out(n, T());
-    for (auto i(0); i < n; ++i)
+    auto const inSize = in.size();
+    auto const kernelSize = kernel.size();
+    auto const outSize = inSize + kernelSize - 1;
+    //std::vector<SampleType> out(n, SampleType());
+    for (auto i(0); i < outSize; ++i)
     {
-        auto const jmn = (i >= ng - 1) ? i - (ng - 1) : 0;
-        auto const jmx = (i < nf - 1) ? i : nf - 1;
+        auto const jmn = (i >= kernelSize - 1) ? i - (kernelSize - 1) : 0;
+        auto const jmx = (i < inSize - 1) ? i : inSize - 1;
         for (auto j(jmn); j <= jmx; ++j)
         {
-            out[i] += (f[j] * g[i - j]);
+            out[i] += (in[j] * kernel[i - j]);
         }
     }
-    return out;
 }
 
 // Reference: https://stackoverflow.com/questions/24518989/how-to-perform-1-dimensional-valid-convolution
-template<typename T>
-std::vector<T> convolution_valid(std::vector<T> const &f, std::vector<T> const &g)
+template<typename SampleType>
+void convolution_valid(const std::vector<SampleType> &in, const std::vector<SampleType> &kernel, std::vector<SampleType> out)
 {
-    int const nf = f.size();
-    int const ng = g.size();
-    std::vector<T> const &min_v = (nf < ng) ? f : g;
-    std::vector<T> const &max_v = (nf < ng) ? g : f;
-    int const n = std::max(nf, ng) - std::min(nf, ng) + 1;
-    std::vector<T> out(n, T());
-    for (auto i(0); i < n; ++i)
+    int const inSize = in.size();
+    int const kernelSize = kernel.size();
+    std::vector<SampleType> const &min_v = (inSize < kernelSize) ? in : kernel;
+    std::vector<SampleType> const &max_v = (inSize < kernelSize) ? kernel : in;
+    int const outSize = std::max(inSize, kernelSize) - std::min(inSize, kernelSize) + 1;
+    //std::vector<SampleType> out(n, T());
+    for (auto i(0); i < outSize; ++i)
     {
         for (int j(min_v.size() - 1), k(i); j >= 0; --j)
         {
@@ -47,7 +48,6 @@ std::vector<T> convolution_valid(std::vector<T> const &f, std::vector<T> const &
             ++k;
         }
     }
-    return out;
 }
 
 // generate a random vector of size n with values in [-1, 1] with std::random
@@ -65,15 +65,70 @@ std::vector<T> generateVectorWithRealNumbers(int n, T min, T max)
     return vectorWithRandomNumbers;
 }
 
-TEST_CASE("Getting started with SIMD Performance Test", "[performance]")
+TEST_CASE("Convolution with/without SIMD Benchmarks", "[performance]")
 {
-    BENCHMARK_ADVANCED("First Steps")
+    auto input = generateVectorWithRealNumbers(16384, -1.0F, 1.0F);
+    auto kernel = generateVectorWithRealNumbers(16, 0.0F, 1.0F);
+
+    BENCHMARK_ADVANCED("convolution_full")
     (Catch::Benchmark::Chronometer meter)
     {
-        auto input = generateVectorWithRealNumbers(65536, -1.0, 1.0);
-        auto kernel = generateVectorWithRealNumbers(16, 0.0, 1.0);
+        auto const outSize = input.size() + kernel.size() - 1;
+        std::vector<float> out(outSize);
+        meter.measure([input, kernel, out]
+                      {
+                          convolution_full(input, kernel, out);
+                          return out; });
+    };
 
-        meter.measure([input, kernel]
-                      { return convolution_full(input, kernel); });
+    BENCHMARK_ADVANCED("convolution_valid")
+    (Catch::Benchmark::Chronometer meter)
+    {
+        const auto inSize = input.size();
+        const auto kernelSize = kernel.size();
+        const auto outSize = std::max(inSize, kernelSize) - std::min(inSize, kernelSize) + 1;
+        std::vector<float> out(outSize);
+
+        meter.measure([input, kernel, out]
+                      {
+                          convolution_valid(input, kernel, out);
+                          return out;
+                      });
+    };
+
+    BENCHMARK_ADVANCED("applyFirFilterSingle")
+    (Catch::Benchmark::Chronometer meter)
+    {
+        fir::FilterInput<float> inputAligned(input, kernel);
+
+        meter.measure([&inputAligned]
+                      { return fir::applyFirFilterSingle(inputAligned); });
+    };
+
+    BENCHMARK_ADVANCED("applyFirFilterInnerLoopVectorization")
+    (Catch::Benchmark::Chronometer meter)
+    {
+        fir::FilterInput<float> inputAligned(input, kernel);
+
+        meter.measure([&inputAligned]
+                      { return fir::applyFirFilterInnerLoopVectorization(inputAligned); });
+    };
+
+    BENCHMARK_ADVANCED("applyFirFilterOuterLoopVectorization")
+    (Catch::Benchmark::Chronometer meter)
+    {
+        fir::FilterInput<float> inputAligned(input, kernel);
+
+        meter.measure([&inputAligned]
+                      { return fir::applyFirFilterOuterLoopVectorization(inputAligned); });
+    };
+
+    BENCHMARK_ADVANCED("applyFirFilterOuterInnerLoopVectorization")
+    (Catch::Benchmark::Chronometer meter)
+    {
+        fir::FilterInput<float> inputAligned(input, kernel);
+
+        meter.measure([&inputAligned]
+                      { return fir::applyFirFilterOuterInnerLoopVectorization(inputAligned); });
     };
 }
