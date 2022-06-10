4f2ce0da3dbc462d0c9689eb7a411ffa3c2c667f
JohT <johnnyt@gmx.at>
2022-06-10 17:35:56 +0200

    add GNU C Vectorization reports and fast math

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -8,6 +8,9 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang|AppleClang") # Using GNU or Clang c
     # -fPIC is needed for position-indipendent code to overcome the following error while linking:
     # "relocation R_X86_64_32S against symbol `stdout@@GLIBC_2.2.5' can not be used when making a shared object; recompile with -fPIC"
     target_compile_options(compiler_options INTERFACE "-fPIC")
+    # This option lets the compiler make aggressive, potentially-lossy assumptions about floating-point math
+    # https://clang.llvm.org/docs/UsersManual.html#cmdoption-ffast-math
+    target_compile_options(compiler_options INTERFACE "-ffast-math")
     # Debugger data in DEBUG mode
     target_compile_options(compiler_options INTERFACE "$<$<CONFIG:Debug>:-g>")
     # Full optimization also in DEBUG mode for benchmarking
@@ -55,7 +58,7 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "Clang|AppleClang") # Using GNU or Clang compi
     target_compile_options(compiler_options INTERFACE "-gcolumn-info")
 endif()
 
-# Enable MSVC Vectorization reports
+# Enable MSVC Vectorization + Reports
 if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC") # Using Microsoft Visual Studio C++
     # Report successfully and unsuccessfully vectorized loops
     target_compile_options(compiler_options INTERFACE "/Qvec-report:2")
@@ -64,6 +67,21 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC") # Using Microsoft Visual Studio C++
     target_compile_options(compiler_options INTERFACE "/arch:AVX2")
 endif()
 
+# Enable GNU C Vectorization + Reports
+if (CMAKE_CXX_COMPILER_ID MATCHES "GNU") # Using GNU C++ Compiler
+    # Enable vectorization (also default at optimization parameter -O3)
+    target_compile_options(compiler_options INTERFACE "-ftree-vectorize")
+    # Enables Vectorization Report Logging
+    # Reference: https://www.gnu.org/software/gcc/projects/tree-ssa/vectorization.html#using
+    # Level 0: No output at all.
+    # Level 1: Report vectorized loops.
+    # Level 2: Also report unvectorized "well-formed" loops and respective reason.
+    # Level 3: Also report alignment information (for "well-formed" loops).
+    # Level 4: Like level 3 + report for non-well-formed inner-loops.
+    # Level 5: Like level 3 + report for all loops.
+    # Level 6: Print all vectorizer dump information (equivalent to former vect-debug-details).
+    target_compile_options(compiler_options INTERFACE "-ftree-vectorizer-verbose=5")
+endif()
 
 # The Memory Sanatizer is only supported by Clang for Linux. 
 # Since GNU C++ is used for Linux, it will never be used.
