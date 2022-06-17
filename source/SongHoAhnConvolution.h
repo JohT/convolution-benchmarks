8f6ddc6810a6ec06eb31949ca8c92438ec7da927
JohT <johnnyt@gmx.at>
2022-06-17 10:32:06 +0200

    add chaowang15 convolution algorithm
    includes minor refactoring of benchmarking code

---
+++ source/SongHoAhnConvolution.h
@@ -0,0 +1,60 @@
+#pragma once
+
+/**
+ * @brief 1d convolution algorithm by Song Ho Ahn
+ * Reference: "Fast Image Convolution in C++" https://github.com/chaowang15/fast-image-convolution-cpp
+ * @author chaowang15
+ */
+namespace chaowang15_convolution
+{
+    /**
+     * @brief 1D convolution
+     * We assume input and kernel signal start from t=0.
+     * Reference: https://github.com/chaowang15/fast-image-convolution-cpp/blob/master/src/convolution.cpp
+     * @param input input signal
+     * @param kernel kernel signal
+     * @param output output signal
+     * @param size input signal size
+     * @param kernel_size kernel signal size
+     * @param stride stride
+     * @author chaowang15
+     */
+    bool convolve1D(const float *in, float *out, int dataSize, const float *kernel, int kernelSize)
+    {
+        int i, j, k;
+
+        // check validity of params
+        if (!in || !out || !kernel)
+        {
+            return false;
+        }
+        if (dataSize <= 0 || kernelSize <= 0)
+        {
+            return false;
+        }
+
+        // start convolution from out[kernelSize-1] to out[dataSize-1] (last)
+        for (i = kernelSize - 1; i < dataSize; ++i)
+        {
+            out[i] = 0;// init to 0 before accumulate
+
+            for (j = i, k = 0; k < kernelSize; --j, ++k)
+            {
+                out[i] += in[j] * kernel[k];
+            }
+        }
+
+        // convolution from out[0] to out[kernelSize-2]
+        for (i = 0; i < kernelSize - 1; ++i)
+        {
+            out[i] = 0;// init to 0 before sum
+
+            for (j = i, k = 0; j >= 0; --j, ++k)
+            {
+                out[i] += in[j] * kernel[k];
+            }
+        }
+
+        return true;
+    }
+}// namespace chaowang15_convolution
--- test/BenchmarkTest.cpp
+++ test/BenchmarkTest.cpp
@@ -1,7 +1,8 @@
 #include "../source/Din0sConvolution.h"
-#include "../source/WilczekConvolution.h"
-#include "../source/MatlabLikeConvolution.h"
 #include "../source/JohTConvolution.h"
+#include "../source/MatlabLikeConvolution.h"
+#include "../source/SongHoAhnConvolution.h"
+#include "../source/WilczekConvolution.h"
 #include "RandomVectorGenerator.h"
 #include "TestVectors.h"
 
@@ -77,16 +78,6 @@ SCENARIO("Convolution Algorithms")
                 din0s::convolve(input.data(), kernel.data(), outPointer, input.size(), kernel.size());
                 REQUIRE_THAT(output, Catch::Matchers::Approx(reference));
             }
-            THEN("Algorithm 'applyFirFilterInnerLoopVectorization' outputs the same result as `applyFirFilterSingle`")
-            {
-                wilczek_convolution::FilterInput<float> inputAligned(input, kernel);
-                wilczek_convolution::FilterInput<float> referenceInputAligned(input, kernel);
-                auto outputFir = wilczek_convolution::applyFirFilterInnerLoopVectorization(inputAligned);
-                auto referenceFir = wilczek_convolution::applyFirFilterSingle(referenceInputAligned);
-
-                //TODO needs quite a bit large margin -> is there something wrong with the implementation?
-                REQUIRE_THAT(outputFir, Catch::Matchers::Approx(referenceFir).margin(0.0005F));
-            }
             THEN("Algorithm 'applyFirFilterSingle' outputs the same result as `convolution_full`")
             {
                 matlab_like::convolution_full(input, kernel, reference);
@@ -95,7 +86,50 @@ SCENARIO("Convolution Algorithms")
                 auto outputFir = wilczek_convolution::applyFirFilterSingle(inputAligned);
 
                 //TODO needs quite a bit large margin -> is there something wrong with the implementation?
-                REQUIRE_THAT(outputFir, Catch::Matchers::Approx(reference).margin(0.0005F));
+                REQUIRE_THAT(outputFir, Catch::Matchers::Approx(reference).margin(0.0002F));
+            }
+            THEN("Algorithm 'applyFirFilterInnerLoopVectorization' outputs the same result as `convolution_full`")
+            {
+                matlab_like::convolution_full(input, kernel, reference);
+
+                wilczek_convolution::FilterInput<float> inputAligned(input, kernel);
+                auto outputFir = wilczek_convolution::applyFirFilterInnerLoopVectorization(inputAligned);
+
+                REQUIRE_THAT(outputFir, Catch::Matchers::Approx(reference));
+            }
+            THEN("Algorithm 'applyFirFilterOuterLoopVectorization' outputs the same result as `convolution_full`")
+            {
+                matlab_like::convolution_full(input, kernel, reference);
+
+                wilczek_convolution::FilterInput<float> inputAligned(input, kernel);
+                auto outputFir = wilczek_convolution::applyFirFilterOuterLoopVectorization(inputAligned);
+
+                REQUIRE_THAT(outputFir, Catch::Matchers::Approx(reference));
+            }
+            THEN("Algorithm 'applyFirFilterOuterInnerLoopVectorization' outputs the same result as `convolution_full`")
+            {
+                matlab_like::convolution_full(input, kernel, reference);
+
+                wilczek_convolution::FilterInput<float> inputAligned(input, kernel);
+                auto outputFir = wilczek_convolution::applyFirFilterOuterInnerLoopVectorization(inputAligned);
+
+                REQUIRE_THAT(outputFir, Catch::Matchers::Approx(reference));
+            }
+            THEN("Algorithm 'convolve1D' outputs the same result as `convolution_full`")
+            {
+                matlab_like::convolution_full(input, kernel, reference);
+
+                // "convolve1D" needs the input to be zero padded at the end.
+                // To be able to multiply the last sample of "input" with the kernel vector,
+                // the input needs further kernel-length - 1 zeroes at the end.
+                auto paddedInput = std::vector<float>(input);
+                auto padding = std::vector<float>(kernel.size() - 1, 0.0);
+                paddedInput.insert(paddedInput.end(), padding.begin(), padding.end());
+
+                auto *outputPointer = std::addressof(output[0]);
+                chaowang15_convolution::convolve1D(paddedInput.data(), outputPointer, paddedInput.size(), kernel.data(), kernel.size());
+
+                REQUIRE_THAT(output, Catch::Matchers::Approx(reference));
             }
         }
     }
@@ -108,26 +142,26 @@ TEST_CASE("Benchmark Convolution Algorithms", "[performance]")
     BENCHMARK_ADVANCED("(JohT) kernelCentricConvolution")
     (Catch::Benchmark::Chronometer meter)
     {
-        auto const outSize = input.size() + kernel.size() - 1;
-        std::vector<float> out(outSize);
-        std::span<const float> inputSpan = std::span(input);
-        std::span<const float> kernelSpan = std::span(kernel);
-        std::span<float> outputSpan = std::span(out);
-        meter.measure([&inputSpan, &kernelSpan, &outputSpan, &out]
+        auto const outputSize = input.size() + kernel.size() - 1;
+        std::vector<float> output(outputSize);
+        const std::span<const float> inputSpan = std::span(input);
+        const std::span<const float> kernelSpan = std::span(kernel);
+        const std::span<float> outputSpan = std::span(output);
+        meter.measure([&inputSpan, &kernelSpan, &outputSpan, &output]
                       {
                           joht_convolution::kernelCentricConvolution(inputSpan, kernelSpan, outputSpan);
-                          return out; });
+                          return output; });
     };
 
     BENCHMARK_ADVANCED("(matlab-like) convolution_full")
     (Catch::Benchmark::Chronometer meter)
     {
-        auto const outSize = input.size() + kernel.size() - 1;
-        std::vector<float> out(outSize);
-        meter.measure([input, kernel, &out]
+        const auto outputSize = input.size() + kernel.size() - 1;
+        std::vector<float> output(outputSize);
+        meter.measure([&input, &kernel, &output]
                       {
-                          matlab_like::convolution_full(input, kernel, out);
-                          return out; });
+                          matlab_like::convolution_full(input, kernel, output);
+                          return output; });
     };
 
     BENCHMARK_ADVANCED("(matlab-like) convolution_valid")
@@ -140,28 +174,32 @@ TEST_CASE("Benchmark Convolution Algorithms", "[performance]")
         paddedInput.insert(paddedInput.begin(), padding.begin(), padding.end());
         paddedInput.insert(paddedInput.end(), padding.begin(), padding.end());
 
-        auto const outSize = input.size() + kernel.size() - 1;
-        std::vector<float> out(outSize);
+        const auto outputSize = input.size() + kernel.size() - 1;
+        std::vector<float> output(outputSize);
 
-        meter.measure([paddedInput, kernel, &out]
+        meter.measure([&paddedInput, &kernel, &output]
                       {
-                          matlab_like::convolution_valid(paddedInput, kernel, out);
-                          return out;
-                      });
+                          matlab_like::convolution_valid(paddedInput, kernel, output);
+                          return output; });
     };
 
     BENCHMARK_ADVANCED("(din0s) convolve")
     (Catch::Benchmark::Chronometer meter)
     {
-        const auto inputSize = input.size();
-        const auto kernelSize = kernel.size();
+        const auto inputSize = static_cast<int>(input.size());
+        const auto inputPointer = input.data();
+
+        const auto kernelSize = static_cast<int>(kernel.size());
+        const auto kernelPointer = kernel.data();
+
         const auto outSize = std::max(inputSize, kernelSize) - std::min(inputSize, kernelSize) + 1;
-        std::vector<float> out(outSize);
-        auto *outPointer = std::to_address(out.begin().base());
-        meter.measure([&input, &kernel, outPointer, inputSize, kernelSize, &out]
+        std::vector<float> output(outSize);
+        auto *outputPointer = std::to_address(output.begin().base());
+
+        meter.measure([inputPointer, kernelPointer, outputPointer, inputSize, kernelSize, &output]
                       {
-                          din0s::convolve(input.data(), kernel.data(), outPointer, inputSize, kernelSize);
-                          return out; });
+                          din0s::convolve(inputPointer, kernelPointer, outputPointer, inputSize, kernelSize);
+                          return output; });
     };
 
     BENCHMARK_ADVANCED("(wilczek) Single")
@@ -198,5 +236,30 @@ TEST_CASE("Benchmark Convolution Algorithms", "[performance]")
 
         meter.measure([&inputAligned]
                       { return wilczek_convolution::applyFirFilterOuterInnerLoopVectorization(inputAligned); });
+    };
+
+    BENCHMARK_ADVANCED("(chaowang15) convolve1D")
+    (Catch::Benchmark::Chronometer meter)
+    {
+        const auto kernelSize = kernel.size();
+
+        // "convolve1D" needs the input to be zero padded at the end.
+        // To be able to multiply the last sample of "input" with the kernel vector,
+        // the input needs further kernel-length - 1 zeroes at the end.
+        auto paddedInput = std::vector<float>(input);
+        const auto padding = std::vector<float>(kernel.size() - 1, 0.0);
+        paddedInput.insert(paddedInput.end(), padding.begin(), padding.end());
+        const auto *paddedInputPointer = paddedInput.data();
+
+        const auto *kernelPointer = kernel.data();
+
+        auto const outputSize = input.size() + kernel.size() - 1;
+        std::vector<float> output(outputSize);
+        auto *outputPointer = std::addressof(output[0]);
+
+        meter.measure([paddedInputPointer, outputSize, kernelPointer, kernelSize, outputPointer, &output]
+                      {
+                          chaowang15_convolution::convolve1D(paddedInputPointer, outputPointer, outputSize, kernelPointer, kernelSize);
+                          return output; });
     };
 }
