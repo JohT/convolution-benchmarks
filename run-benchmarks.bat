3deaa312d3454e423bd033e04989b2b16add2c08
JohT <johnnyt@gmx.at>
2022-07-30 21:23:25 +0200

    move existing script to script folder

--- run-benchmarks.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-mkdir .\build\benchmark
-.\build\test\ConvolutionBenchmarksTests [performance] --reporter XML::out=.\build\benchmark\benchmark-report.xml --reporter console::out=-::colour-mode=ansi
--- run-benchmarks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mkdir -p ./build/benchmark
-./build/test/ConvolutionBenchmarksTests [performance] --reporter XML::out=./build/benchmark/benchmark-report.xml --reporter console::out=-::colour-mode=ansi
